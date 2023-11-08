4843b0d371e4aa3a25cba9d28697b25e4693d8ee
dpark93 <d.park.official@gmail.com>
2023-11-08 14:11:16 -0700

    challnege complete

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const cookieParser = require('cookie-parser')
 const app = express();
+app.use(cookieParser())
+app.use(express.json())
+
 
 app.get('/', (req, res)=>{
     res.status(200).send("Hello World!")
 })
+
+
+app.get('/login', (req, res) => {
+    var user = "Durand"
+    res.cookie("name", user); // input ?? 
+    res.send(`You Just Saved a cookie with name ${req.cookies.name}`)
+  })
+  
+  app.get('/hello', (req, res) => {
+    res.send(`Welcome ${req.cookies.name}`)
+  })
+  
 
 module.exports = app;
